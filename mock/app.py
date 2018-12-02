b1764819cbe526af6327edde16cf2fca26501bb2
c10t <c10t@fa.ke>
2018-12-02 23:52:48 +0900

    display sample image

--- mock/app.py
+++ mock/app.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", contents=contents())
+
+
+def contents():
+    lines = []
+    for i in range(3):
+        line = []
+        for j in range(4):
+            item = {}
+            item['title'] = f"Title-{i}{j}"
+            line.append(item)
+        lines.append(line)
+    return lines
 
 
 if __name__ == '__main__':
