9d78509699d41110f55daabf7a3fc9fe1ba117c7
c10t <c10t@fa.ke>
2019-01-13 22:15:04 +0900

    add header and footer

--- front-vue/pictwise/src/App.vue
+++ front-vue/pictwise/src/App.vue
@@ -1,7 +1,26 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="header">
+      <div class="pure-menu pure-menu-horizontal">
+        <a class="pure-menu-heading" href="">Gallery</a>
+        <ul class="pure-menu-list">
+          <li class="pure-menu-item pure-menu-selected">
+            <a class="pure-menu-link" href="#">Home</a>
+          </li>
+          <li class="pure-menu-item">
+            <a class="pure-menu-link" href="#upload-image">Upload</a>
+          </li>
+          <li class="pure-menu-item">
+            <a class="pure-menu-link" href="#">Settings</a>
+          </li>
+          <li class="pure-menu-item">
+            <a class="pure-menu-link" href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
     <router-view/>
+    <div class="footer">piyo</div>
   </div>
 </template>
 
@@ -19,5 +38,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.footer {
+  color: #FFFFFF;
+  font-size: 87.5%;
+  border-top: 1px solid #eee;
+  margin-top: 1em;
+  padding: 1em;
+  background: rgb(49, 49, 49);
 }
 </style>
